eb89806c84788cbf6594767c8391e011017a7229
ricardotran92 <quanghung.850621@gmail.com>
2024-03-14 22:22:30 +0700

    Create development nodemon mode

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv';
 // Một thư viện của JavaScript load các biến môi trường từ tập tin '.env' vào application's runtime environment
 dotenv.config({ path: "backend/config/config.env" });
 
-// app instance đăng ký routes và nghe ở port 3000.
+// app instance đăng ký routes và listen ở port 3000.
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
