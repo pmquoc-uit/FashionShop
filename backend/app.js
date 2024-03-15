df04b1f10ca40c10b6724a16e46486f056c160ae
ricardotran92 <quanghung.850621@gmail.com>
2024-03-15 22:29:43 +0700

    Set up project

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,12 @@ const app = express();
 // Một thư viện của JavaScript load các biến môi trường từ tập tin '.env' vào application's runtime environment
 import dotenv from "dotenv";
 
+import { connectDatabase } from "./config/dbConnect.js"; // tự hiện khi gõ lệnh connectDatabase()
+
 dotenv.config({ path: "backend/config/config.env" });
+
+// Connect với database
+connectDatabase();
 
 // Import tất cả các routes (đường dẫn)
 import productRoutes from "./routes/products.js";
@@ -17,6 +22,6 @@ app.use("/api", productRoutes);
 // app instance đăng ký routes và listen ở port 3000.
 app.listen(process.env.PORT, () => {
     console.log(
-        `Server is running on port ${process.env.PORT} in mode ${process.env.NODE_ENV}`
+        `Server đang chạy ở port ${process.env.PORT} ở chế độ ${process.env.NODE_ENV}`
     );
 });
