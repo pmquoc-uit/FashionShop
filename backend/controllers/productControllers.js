2dbe43754ac81a498c85f3df066fce0613e20c42
Pham Tien Nam <tiennam27@gmail.com>
2024-03-18 10:56:20 +0700

    update seeder. controller, route them xoa sua sp

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,9 +1,82 @@
+import Product from "../models/product.js"; // Import model Product từ đường dẫn ../models/product.js
+
 /*
 Hàm điều khiển (controller functions) cho các file routes và xác định route
 Các điều khiển và các logic cho tài nguyên sản phẩm (product resource)
 */
-export const getProducts = async (req, res) => {
-  res.status(200).json({
-    message: "Tất cả sản phẩm",
+export const getProducts = async (req, res) => { // Khai báo hàm điều khiển getProducts nhận req và res làm tham số
+    const products = await Product.find(); // Tìm tất cả các sản phẩm từ cơ sở dữ liệu và gán cho biến products
+    res.status(200).json({ // Trả về mã trạng thái 200 và dữ liệu JSON chứa danh sách sản phẩm
+        products, // Trả về danh sách sản phẩm
+    }); 
+};
+
+//Tạo sản phẩm mới với đường dẫn => /api/admin/products
+export const newProduct = async (req, res) => { // Khai báo hàm điều khiển newProduct nhận req và res làm tham số
+    const product = await Product.create(req.body); // Tạo một sản phẩm mới từ dữ liệu được gửi trong yêu cầu và gán cho biến product
+    res.status(200).json({ // Trả về mã trạng thái 200 và dữ liệu JSON chứa thông tin sản phẩm mới được tạo
+        product, // Trả về thông tin của sản phẩm mới được tạo
+    });
+};
+
+
+//Tìm 1 sản phẩm mới với đường dẫn => /products/:id
+export const getProductDetail = async (req, res) => { // Khai báo hàm điều khiển newProduct nhận req và res làm tham số
+    const product = await Product.findById(req?.params?.id ); // Tạo một sản phẩm mới từ dữ liệu được gửi trong yêu cầu và gán cho biến product
+
+    if(!product) {
+        return res.status(404).json({
+            error: "Sản phẩm không tồn tại",
+        });        
+    };
+
+    res.status(200).json({ // Trả về mã trạng thái 200 và dữ liệu JSON chứa thông tin sản phẩm mới được tạo
+        product, // Trả về thông tin của sản phẩm mới được tạo
+    });
+};
+
+//Update chi tiết sản phẩm mới với đường dẫn => /products/:id
+export const updateProduct = async (req, res) => { // Khai báo hàm điều khiển newProduct nhận req và res làm tham số
+    let product = await Product.findById(req?.params?.id ); // Tạo một sản phẩm mới từ dữ liệu được gửi trong yêu cầu và gán cho biến product
+
+    if(!product) {
+        return res.status(404).json({
+            error: "Sản phẩm không tồn tại",
+        });         
+    };
+
+    product = await Product.findByIdAndUpdate(req?.params?.id, req.body, {
+        new: true,
+    });
+
+
+    res.status(200).json({ // Trả về mã trạng thái 200 và dữ liệu JSON chứa thông tin sản phẩm mới được tạo
+        product, // Trả về thông tin của sản phẩm mới được tạo
+    });
+};
+
+
+
+//Xóa sản phẩm với đường dẫn => /products/:id
+export const deleteProduct = async (req, res) => { // Khai báo hàm điều khiển newProduct nhận req và res làm tham số
+  const product = await Product.findById(req?.params?.id ); // Tạo một sản phẩm mới từ dữ liệu được gửi trong yêu cầu và gán cho biến product
+
+  if(!product) {
+    //throw new error()
+      return res.status(404).json({
+          error: "Sản phẩm không tồn tại",
+      });         
+  };
+
+  await product.deleteOne();
+
+
+  res.status(200).json({ // Trả về mã trạng thái 200 và dữ liệu JSON chứa thông tin sản phẩm mới được tạo
+      message: "Đã xóa sản phẩm",
   });
 };
+
+
+
+
+
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -4,7 +4,13 @@
 Routes () cho tài nguyên sản phẩm
 */
 import express from 'express'
-import { getProducts } from '../controllers/productControllers.js'; // tự động xuất hiện khi gõ syntax get(getProducts)
+import { 
+    getProducts, 
+    newProduct, 
+    getProductDetail, 
+    updateProduct,
+    deleteProduct 
+} from '../controllers/productControllers.js'; // tự động xuất hiện khi gõ syntax get(getProducts)
 
 // Tạo một đối tượng Router của Express mới để định nghĩa các tuyến đường (routes) cho ứng dụng.
 const router = express.Router()
@@ -12,6 +18,18 @@ const router = express.Router()
 // Router route(dẫn) đến mục "/products" để get (nhận request) và đưa vào controller function (hàm điều khiển)
 router.route("/products").get(getProducts);
 
+//Router route(dẫn) đến mục "/products" để post sản phẩm mới
+router.route("/admin/products").post(newProduct);
+
+router.route("/products/:id").get(getProductDetail);
+
+//Router route(dẫn) đến mục "/products" để sửa sản phẩm mới
+router.route("/products/:id").put(updateProduct);
+
+//Router route(dẫn) đến mục "/products" để sửa sản phẩm mới
+router.route("/products/:id").delete(deleteProduct);
+
+ 
 // để sử dụng trong các files. Khi 1 tệp (app.js) muốn import từ 1 module khác (product.js) thì cần export dữ liệu từ module đó (tương tự return)
 export default router;
 
