3dddc29950701ce6b0f10cbd505caaf257881b30
ricardotran92 <quanghung.850621@gmail.com>
2024-03-18 18:43:05 +0700

    Raw Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
+import Home from './component/Home';
 import Footer from "./component/layout/Footer";
 import Header from "./component/layout/Header";
+
+
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      {/* Page header từ ./component/layout/Footer */}
       <Header/>
+
+      <div className="container">
+        <Home/>
+      </div>
+      
+
+      
+  
+
+      {/* Page Footer từ ./component/layout/Header*/}
       <Footer/>
     </div>
   );
--- frontend/src/component/layout/Footer.js
+++ frontend/src/component/layout/Footer.js
@@ -2,10 +2,132 @@ import React from 'react'
 
 const Footer = () => {
   return (
+
+    
+
     <footer class="py-1 pt-5">
-      <p class="text-center mt-1 fw-bold">
-      Copyright © 2022 - 2024 FashionShop.
-      </p>
+
+      <a href="javascript:void(0)" id="navBackToTop" aria-label="Back to top" >
+        <div className="navFooterBackToTop">
+        <span className="navFooterBackToTopText">
+          Back to top
+        </span>
+        </div>
+      </a>
+
+      
+              <div className="footer-top">
+                <div className="container">
+                  <div className="row">
+                    <div className="col w-full lg:w-1/2">
+                      <div className='Module Module-318'><div className='ModuleContent'><div className="title">Đăng ký nhận bản tin</div>
+      <div className="caption">Đăng ký ngay đễ nhận thông tin khuyến mãi mới nhất</div></div></div>
+                    </div>
+                    <div className="col w-full lg:w-1/2">
+                      <div className='Module Module-319'><div id='ctl00_mdl319_ctl00_subscribe1_pnlSubscribeBody' className='subscribe'>
+
+          
+          <div id="ctl00_mdl319_ctl00_subscribe1_UpdatePanel1">
+        
+                  <div id='ctl00_mdl319_ctl00_subscribe1_pnlSubscribe' className='subscribefrm'>
+
+                      <input onfocus="javascript:watermarkEnter(this, &#39;&#39;);" onblur="javascript:watermarkLeave(this, &#39;&#39;);" name="ctl00$mdl319$ctl00$subscribe1$txtEmail" type="text" id="ctl00_mdl319_ctl00_subscribe1_txtEmail" title="Nhập địa chỉ email hợp lệ." className="subscribeemail" placeholder="Địa chỉ email" />
+                      
+                      
+                      <button onclick="__doPostBack('ctl00$mdl319$ctl00$subscribe1$btnSubscribe','')" id="ctl00_mdl319_ctl00_subscribe1_btnSubscribe" className="subscribebutton" ValidationGroup="subscribe">
+                          Đăng ký
+                      </button>
+                      <span id="ctl00_mdl319_ctl00_subscribe1_reqEmail" className="fa fa-exclamation-triangle" style={{display:"none;"}}>Vui lòng nhập email.</span>
+                      <span id="ctl00_mdl319_ctl00_subscribe1_regexEmail" className="fa fa-exclamation-triangle" style={{display:"none;"}}>Email không hợp lệ.</span>
+                  
+      </div>
+                  
+                  
+              
+      </div>
+
+      </div></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="footer-bottom">
+                <div className="container">
+                  <div className="row">
+                    <div className="col w-full sm:w-1/2 lg:w-3/12  darken-bg">
+                      <div className='Module Module-419'><div className='ModuleContent'><div className="title"><a href="https://anphuocgroup.com"><span style={{color: "#ff0000;"}}>An Phước Group</span></a></div>
+      <p>
+      <span>100/11-12 An Dương Vương, P.9, Q.5, TP. Hồ Chí Minh, Việt Nam</span></p>
+      <ul>
+          <li><span style={{fontSize: "13px;"}}>GPKD Số: 0301241545</span></li>
+          <li><span style={{fontSize: "13px;"}}>Ngày cấp: 26/04/1993GPKD Số: 0301241545</span></li>
+          <li><span style={{fontSize: "13px;"}}><a href="tel:1800 888 618">Tư vấn mua hàng: 1800 888 618</a>&nbsp;(Phím 1)</span></li>
+          <li><span style={{fontSize: "13px;"}}>Văn phòng: +84.28.3835.0059</span></li>
+          <li><span style={{fontSize: "13px;"}}>CSKH: 1800 888 618 (Phím 3)</span></li>
+          <li><span style={{fontSize: "13px;"}}>Fax: +84.28.3835.0058</span></li>
+          <li><span style={{fontSize: "13px;"}}>Email: cskh@anphuoc.com.vn</span></li>
+      </ul></div></div>
+                      {/* <!-- <div className='Module Module-320'><div className='ModuleContent'><div className="title  toggle-footer"><span>Sản phẩm</span><em className="lnr lnr-chevron-down"></em></div>
+      <ol>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/promotion" title="Khuyến Mãi">Khuyến Mãi</a></li>
+      </ol></div></div> --> */}
+
+
+                    </div>
+                    <div className="col w-full sm:w-1/2 lg:w-3/12">
+                      <div className='Module Module-321'><div className='ModuleContent'><div className="title toggle-footer"><span>Chăm sóc khách hàng</span><em className="lnr lnr-chevron-down"></em></div>
+      <ol>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/cham-soc-khach-hang/huong-dan-mua-hang" title="Hướng dẫn mua hàng">Hướng dẫn mua hàng</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/cham-soc-khach-hang/huong-dan-thanh-toan-1" title="Hướng dẫn thanh toán">Hướng dẫn thanh toán</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/cham-soc-khach-hang/huong-dan-chon-size" title="Hướng dẫn chọn kích cỡ An Phước - Pierre Cardin">Hướng dẫn chọn kích cỡ An Phước - Pierre Cardin</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/cham-soc-khach-hang/huong-dan-chon-kich-c-anamai-bonjour" title="Hướng dẫn chọn kích cỡ Anamai - Bonjour">Hướng dẫn chọn kích cỡ Anamai - Bonjour</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/cham-soc-khach-hang/thoi-gian-giao-hang" title="Thời gian giao hàng">Thời gian giao hàng</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/cham-soc-khach-hang/chinh-sach-bao-mat" title="Chính sách bảo mật">Chính sách bảo mật</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/cham-soc-khach-hang/quy-dinh-doi-tra" title="Chính sách đổi hàng">Chính sách đổi hàng</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/cham-soc-khach-hang/hoi-dap" title="Hỏi đáp">Hỏi đáp</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/cham-soc-khach-hang/cam-ket-chat-luong" title="Cam kết chất lượng">Cam kết chất lượng</a></li>
+      </ol></div></div>
+                    </div>
+                    <div className="col w-full sm:w-1/2 lg:w-3/12">
+                      <div className='Module Module-322'><div className='ModuleContent'><div className="title  toggle-footer"><span>Liên kết nhanh</span><em className="lnr lnr-chevron-down"></em></div>
+      <ol>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/thu-vien-anh-2" title="Bộ sưu tập">Bộ sưu tập</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/tin-khuyen-mai" title="Tin khuyến mãi">Tin khuyến mãi</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/tin-thoi-trang" title="Tin thời trang">Tin thời trang</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/tin-tuc-su-kien" title="Tin tức - Sự kiện">Tin tức - Sự kiện</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/he-thong-cua-hang" title="Hệ thống cửa hàng An Phước - Pierre Cardin">Hệ thống cửa hàng An Phước - Pierre Cardin</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/he-thong-cua-hang-anamai-bonjour" title="Hệ thống cửa hàng Anamai - Bonjour">Hệ thống cửa hàng Anamai - Bonjour</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/lien-he" title="Liên hệ">Liên hệ</a></li>
+        <li><a target="_self" href="https://anphuocgroup.com/tuyen-dung/co-hoi-nghe-nghiep" title="Tuyển dụng">Tuyển dụng</a></li>
+        <li><a target="_self" href="https://www.anphuoc.com.vn/thong-bao-canh-giac-gia-mao-thuong-hieu--an-phuoc-pierre-cardin-1" title="Cảnh giác giả mạo An Phước - Pierre Cardin">Cảnh giác giả mạo An Phước - Pierre Cardin</a></li>
+      </ol></div></div>
+
+                    </div>
+                    <div className="col w-full sm:w-1/2 lg:w-3/12">
+                      <div className='Module Module-323'><div className='ModuleContent'><div className="title" style={{position: "relative; top: 3px; text-align: left;"}}>
+      KẾT NỐI VỚI CHÚNG TÔI
+      </div></div></div>
+                      <div className="social-network">
+                        <div className='Module Module-324'><div className='ModuleContent'>&nbsp; &nbsp;&nbsp;<a href=" https://www.facebook.com/AnPhuoc.PierreCardin.Official "><em className="fab fa-facebook-f"></em></a><a href="https://zalo.me/1865611096449147467"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="25" height="25" viewbox="0 0 172 172" style={{fill:"#000000;"}}><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style={{mixBlendMode: "normal;"}}>
+      <path d="M0,172v-172h172v172z" fill="none"></path>
+      <g fill="#ffffff">
+      
+      </g></g></svg></a><a href="https://www.youtube.com/channel/UCTc-4zM5fRI4FrSKRjdwphw "><em className="fab fa-youtube"></em></a>
+      <a href="https://www.instagram.com/anphuoc.pierrecardin"><em className="fab fa-instagram"></em></a>
+      <a href="https://www.linkedin.com/company/anphuoc"><em className="fab fa-linkedin-in"></em></a></div></div>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+
+
+
+        <p class="text-center mt-1 fw-bold">
+        Copyright © 2024 FashionShop.
+        </p>
     </footer>
     
   )
