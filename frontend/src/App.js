23ccf78acd3ce635709319bb3577df70c90aecfa
pmquoc-uit <pmquoc.uit@gmail.com>
2024-03-22 21:23:46 +0700

    Implement React Router DOM

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
+
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+
 import Home from './component/Home';
 import Footer from "./component/layout/Footer";
 import Header from "./component/layout/Header";
@@ -9,21 +12,25 @@ import Header from "./component/layout/Header";
 
 function App() {
   return (
-    <div className="App">
-      {/* Page header từ ./component/layout/Footer */}
-      <Header/>
+    <Router>
+      <div className="App">
+        {/* Page header từ ./component/layout/Footer */}
+        <Header/>
 
-      <div className="container">
-        <Home/>
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+        
+
+        
+    
+
+        {/* Page Footer từ ./component/layout/Header*/}
+        <Footer/>
       </div>
-      
-
-      
-  
-
-      {/* Page Footer từ ./component/layout/Header*/}
-      <Footer/>
-    </div>
+    </Router>
   );
 }
 
